e68af3e55d70b0d59fb83fdedab91e422060f075
Aswin Mohan <git.aswinmohan@gmail.com>
2018-08-15 21:58:52 +0530

    Setup Skeleton

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import MainPage from './components/mainPage'
+
+const App = (props) => {
+  return (
+    <MainPage />
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/mainPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class MainPage extends React.Component {
+  render () {
+    return(
+    <h1>Kerala Flood Relief </h1>
+  );
+  }
+}
+
+export default MainPage;
