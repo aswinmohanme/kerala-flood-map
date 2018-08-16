58e6a1474e63212c0cd130384776a3808e2edeef
Aswin Mohan <git.aswinmohan@gmail.com>
2018-08-16 10:05:49 +0530

    FIx Marker Support

--- src/components/mainPage/index.js
+++ src/components/mainPage/index.js
@@ -4,11 +4,65 @@ import PropTypes from "prop-types";
 import MainPageMap from "./mainPageMap";
 
 class MainPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      position: {
+        lat: 10,
+        lng: 0
+      },
+      markers: []
+    };
+
+    this.getCurrentLocation = this.getCurrentLocation.bind(this);
+    this.render = this.render.bind(this);
+  }
+
+  getCurrentLocation() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          this.setState(prevState => ({
+            position: {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            },
+            markers: [
+              ...prevState.markers,
+              {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+              }
+            ]
+          }));
+        },
+        error => {
+          alert("An Error Occured Getting Your Location, Error: " + error);
+        }
+      );
+    } else {
+      alert("Location is Not Supported");
+    }
+  }
+
   render() {
+    console.log(this.state.markers);
     return (
       <div>
-        <h1>Hey</h1>
+        <div className="pa3 flex items-center justify-between">
+          <h3 className="">Kerala Flood Map</h3>
+          <a
+            onClick={this.getCurrentLocation}
+            href="#"
+            className="black bg-red link pa2 br2"
+          >
+            I'm Stuck
+          </a>
+        </div>
         <MainPageMap
+          position={this.state.position}
+          markers={this.state.markers}
           containerElement={<div style={{ height: "100vh", width: "100vw" }} />}
           mapElement={<div style={{ height: `100%`, width: "100%" }} />}
         />
--- src/components/mainPage/mainPageMap.js
+++ src/components/mainPage/mainPageMap.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { withGoogleMap, GoogleMap } from "react-google-maps";
+import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 
 const MainPageMap = withGoogleMap(props => (
-  <GoogleMap
-    defaultCenter={{ lat: 40.756795, lng: -73.954298 }}
-    defaultZoom={13}
-  />
+  <GoogleMap center={props.position} defaultZoom={13}>
+    {props.markers.map(marker => (
+      <Marker key={marker.lat + marker.lng} position={marker} />
+    ))}
+  </GoogleMap>
 ));
 
 export default MainPageMap;
