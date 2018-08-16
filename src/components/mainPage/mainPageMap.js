962b0b5199b4b27c87187d150ae7cefa03c1ea9e
Aswin Mohan <git.aswinmohan@gmail.com>
2018-08-16 11:15:08 +0530

    Reduce Map Zoom Level

--- src/components/mainPage/mainPageMap.js
+++ src/components/mainPage/mainPageMap.js
@@ -2,7 +2,7 @@ import React from "react";
 import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 
 const MainPageMap = withGoogleMap(props => (
-  <GoogleMap center={props.position} defaultZoom={13}>
+  <GoogleMap center={props.position} defaultZoom={7}>
     {props.markers.map(marker => (
       <Marker key={marker.lat + marker.lng} position={marker} />
     ))}
