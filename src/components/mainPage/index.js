dcbdef14442d586b8c4c82baf144b05227dd2040
Bodhish Thomas <bodhish@gmail.com>
2018-08-17 03:34:04 +0530

    Add filter for people who needs rescue

--- src/components/mainPage/index.js
+++ src/components/mainPage/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-
 import MainPageMap from "./mainPageMap";
 
 class MainPage extends React.Component {
@@ -9,18 +8,30 @@ class MainPage extends React.Component {
 
     this.state = {
       position: null,
-      markers: []
+      markers: [],
+      markersNeedRescue: [],
+      needsRescue: false
     };
 
     this.render = this.render.bind(this);
     this.locateMe = this.locateMe.bind(this);
+    this.filterRescue = this.filterRescue.bind(this);
   }
 
   async componentDidMount() {
     const resp = await fetch("/data");
     const markers = await resp.json();
-
     this.setState({ markers: markers });
+  }
+
+  filterRescue() {
+    const needRescueGroup = this.state.markers.filter(
+      marker => marker.needrescue === true
+    );
+    this.setState(prevState => ({
+      needsRescue: !prevState.needsRescue,
+      markersNeedRescue: needRescueGroup
+    }));
   }
 
   locateMe() {
@@ -58,14 +69,28 @@ class MainPage extends React.Component {
           </div>
         </div>
         <div className="flex items-center pl3">
-          <p className="f6 mr2">Red: Needs Rescue</p>
+          <a
+            href="#"
+            onClick={this.filterRescue}
+            className={
+              !this.state.needsRescue
+                ? "link red ba pa2 mr2 br2"
+                : "link bg-red white pa2 mr2 br2"
+            }
+          >
+            Red: Needs Rescue
+          </a>
           <p className="f6 mh2">Green: Request Made For Other</p>
           <p className="f6 mh2">Blue: Generic Request</p>
         </div>
         <MainPageMap
           position={this.state.position || [10, 76]}
           zoomLevel={this.state.position === null ? 7 : 13}
-          markers={this.state.markers}
+          markers={
+            !this.state.needsRescue
+              ? this.state.markers
+              : this.state.markersNeedRescue
+          }
         />
       </div>
     );
@@ -75,31 +100,32 @@ class MainPage extends React.Component {
 MainPage.propTypes = {
   position: PropTypes.arrayOf(PropTypes.number.isRequired),
   zoomLevel: PropTypes.number,
-  markers: PropTypes.arrayOf(PropTypes.shape({
-    map: PropTypes.shape({
-      latlng: PropTypes.string.isRequired,
-      requestee: PropTypes.string.isRequired,
-      needrescue: PropTypes.bool,
-      detailrescue: PropTypes.string,
-      is_request_for_others: PropTypes.bool,
-      location: PropTypes.string,
-      requestee_phone: PropTypes.string,
-      needfood: PropTypes.bool,
-      detailfood: PropTypes.string,
-      needcloth: PropTypes.bool,
-      detailcloth: PropTypes.string,
-      needwater: PropTypes.string,
-      detailwater: PropTypes.string,
-      needkit_util: PropTypes.string,
-      detailkit_util: PropTypes.string,
-      needtoilet: PropTypes.string,
-      detailtoilet: PropTypes.string,
-      needmed: PropTypes.string,
-      detailmed: PropTypes.string,  
-      needothers: PropTypes.string
+  markers: PropTypes.arrayOf(
+    PropTypes.shape({
+      map: PropTypes.shape({
+        latlng: PropTypes.string.isRequired,
+        requestee: PropTypes.string.isRequired,
+        needrescue: PropTypes.bool,
+        detailrescue: PropTypes.string,
+        is_request_for_others: PropTypes.bool,
+        location: PropTypes.string,
+        requestee_phone: PropTypes.string,
+        needfood: PropTypes.bool,
+        detailfood: PropTypes.string,
+        needcloth: PropTypes.bool,
+        detailcloth: PropTypes.string,
+        needwater: PropTypes.string,
+        detailwater: PropTypes.string,
+        needkit_util: PropTypes.string,
+        detailkit_util: PropTypes.string,
+        needtoilet: PropTypes.string,
+        detailtoilet: PropTypes.string,
+        needmed: PropTypes.string,
+        detailmed: PropTypes.string,
+        needothers: PropTypes.string
+      })
     })
-  }))
+  )
 };
-
 
 export default MainPage;
