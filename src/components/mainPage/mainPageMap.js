9c0e7927aefadeb486702121caadbe65c5cb9c0c
Aswin Mohan <git.aswinmohan@gmail.com>
2018-08-16 20:28:33 +0530

    Render Out Markers and Prepare for Build

--- src/components/mainPage/mainPageMap.js
+++ src/components/mainPage/mainPageMap.js
@@ -21,7 +21,7 @@ const MainPageMap = props => (
     />
     {props.markers.map(
       (marker, index) =>
-        isValidCoords(marker.latlng) ? (
+        isValidCoords(marker.latlng) && !marker.is_request_for_others ? (
           <Marker position={returnCoord(marker.latlng)}>
             <Popup>
               <div className="">
@@ -29,22 +29,24 @@ const MainPageMap = props => (
                 <p className="f6">Location : {marker.location}</p>
                 <p className="f6">Phone Number : {marker.requestee_phone}</p>
                 {marker.needfood && (
-                  <p className="f6">Food : {marker.detailfood}</p>
+                  <p className="f6">Need Food : {marker.detailfood}</p>
                 )}
                 {marker.needcloth && (
-                  <p className="f6">Cloth : {marker.detailcloth}</p>
+                  <p className="f6">Need Cloth : {marker.detailcloth}</p>
                 )}
                 {marker.needwater && (
-                  <p className="f6">Water : {marker.detailwater}</p>
+                  <p className="f6">Need Water : {marker.detailwater}</p>
                 )}
                 {marker.needkit_util && (
-                  <p className="f6">Kitchen Utils : {marker.detailkit_util}</p>
+                  <p className="f6">
+                    Need Kitchen Utils : {marker.detailkit_util}
+                  </p>
                 )}
                 {marker.needtoilet && (
-                  <p className="f6">Toilet : {marker.detailtoilet}</p>
+                  <p className="f6">Need Toilet : {marker.detailtoilet}</p>
                 )}
                 {marker.needmed && (
-                  <p className="f6">Medical : {marker.detailmed}</p>
+                  <p className="f6">Need Medical : {marker.detailmed}</p>
                 )}
                 <p className="f6">{marker.needothers}</p>
               </div>
