cbbf98101a3d94be4a1e0332c0ddb0ad91321234
Aswin Mohan <git.aswinmohan@gmail.com>
2018-08-17 07:17:50 +0530

    Refactor Out Marker Popup Component

--- src/components/mainPage/mainPageMap.js
+++ src/components/mainPage/mainPageMap.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-markercluster";
-
 import L from "leaflet";
 
 import RedMarker from "../../assets/red-dot.png";
 import BlueMarker from "../../assets/blue-dot.png";
 import GreenMarker from "../../assets/green-dot.png";
 import MarkerShadow from "../../assets/marker-shadow.png";
+
+import MarkerPopup from "./markerPopup";
 require("react-leaflet-markercluster/dist/styles.min.css");
 
 function isValidCoords(latlng) {
@@ -69,51 +70,7 @@ const MainPageMap = props => (
               }
               position={returnCoord(marker.latlng)}
             >
-              <Popup>
-                <div className="">
-                  <h3 className="f3">{marker.requestee}</h3>
-                  {marker.needrescue && (
-                    <p className="f6">Need Rescue : {marker.detailrescue}</p>
-                  )}
-                  {marker.is_request_for_others ? (
-                    <p className="f6">
-                      Request for Others at {marker.location}
-                    </p>
-                  ) : (
-                    <p className="f6">Location : {marker.location}</p>
-                  )}
-                  <p className="f6">Phone Number : {marker.requestee_phone}</p>
-                  {marker.needfood && (
-                    <p className="f6">Need Food {marker.detailfood}</p>
-                  )}
-                  {marker.needcloth && (
-                    <p className="f6">Need Cloth {marker.detailcloth}</p>
-                  )}
-                  {marker.needwater && (
-                    <p className="f6">Need Water {marker.detailwater}</p>
-                  )}
-                  {marker.needkit_util && (
-                    <p className="f6">
-                      Need Kitchen Utils {marker.detailkit_util}
-                    </p>
-                  )}
-                  {marker.needtoilet && (
-                    <p className="f6">Need Toilet {marker.detailtoilet}</p>
-                  )}
-                  {marker.needmed && (
-                    <p className="f6">Need Medical {marker.detailmed}</p>
-                  )}
-                  <p className="f6">{marker.needothers}</p>
-                  <a
-                    href={"http://maps.google.com/maps?q=loc:" + marker.latlng}
-                  >
-                    Navigate to the location
-                  </a>
-                  <p className="f6">
-                    Location accuracy: {marker.latlng_accuracy}
-                  </p>
-                </div>
-              </Popup>
+              <MarkerPopup marker={marker} />
             </Marker>
           ) : null
       )}
