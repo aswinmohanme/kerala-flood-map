ca4a8f376bb251d157a7c5e415eae8ab6e76bdbd
Aswin Mohan <git.aswinmohan@gmail.com>
2018-08-17 10:05:55 +0530

    Add Link to Flooded Roads
    Fixes: #15

--- src/components/mainPage/index.js
+++ src/components/mainPage/index.js
@@ -9,11 +9,22 @@ class MainPage extends React.Component {
 
     this.state = {
       position: null,
+      zoom: 7,
       markers: []
     };
 
     this.render = this.render.bind(this);
     this.locateMe = this.locateMe.bind(this);
+
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.setState({
+          position: [position.coords.latitude, position.coords.longitude]
+        });
+      });
+    } else {
+      alert("Error Getting Your Location");
+    }
   }
 
   async componentDidMount() {
@@ -27,7 +38,8 @@ class MainPage extends React.Component {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(position => {
         this.setState({
-          position: [position.coords.latitude, position.coords.longitude]
+          position: [position.coords.latitude, position.coords.longitude],
+          zoom: 13
         });
       });
     } else {
@@ -42,11 +54,24 @@ class MainPage extends React.Component {
           <h3 className="">Kerala Flood Map</h3>
           <div>
             <a
+              href={
+                this.state.position
+                  ? `https://www.microid.in/keralaflood/#12/${
+                      this.state.position[0]
+                    }/${this.state.position[1]}`
+                  : "https://www.microid.in/keralaflood/"
+              }
+              target="blank"
+              className="link black ba pa2 mr2 br2"
+            >
+              Check Roads
+            </a>
+            <a
               href="#"
               onClick={this.locateMe}
               className="link black ba pa2 mr2 br2"
             >
-              Near Me
+              Locate Me
             </a>
             <a
               href="https://keralarescue.in/request/"
@@ -64,7 +89,7 @@ class MainPage extends React.Component {
         </div>
         <MainPageMap
           position={this.state.position || [10, 76]}
-          zoomLevel={this.state.position === null ? 7 : 13}
+          zoomLevel={this.state.zoom}
           markers={this.state.markers}
         />
       </div>
@@ -75,31 +100,32 @@ class MainPage extends React.Component {
 MainPage.propTypes = {
   position: PropTypes.arrayOf(PropTypes.number.isRequired),
   zoomLevel: PropTypes.number,
-  markers: PropTypes.arrayOf(PropTypes.shape({
-    map: PropTypes.shape({
-      latlng: PropTypes.string.isRequired,
-      requestee: PropTypes.string.isRequired,
-      needrescue: PropTypes.bool,
-      detailrescue: PropTypes.string,
-      is_request_for_others: PropTypes.bool,
-      location: PropTypes.string,
-      requestee_phone: PropTypes.string,
-      needfood: PropTypes.bool,
-      detailfood: PropTypes.string,
-      needcloth: PropTypes.bool,
-      detailcloth: PropTypes.string,
-      needwater: PropTypes.string,
-      detailwater: PropTypes.string,
-      needkit_util: PropTypes.string,
-      detailkit_util: PropTypes.string,
-      needtoilet: PropTypes.string,
-      detailtoilet: PropTypes.string,
-      needmed: PropTypes.string,
-      detailmed: PropTypes.string,  
-      needothers: PropTypes.string
+  markers: PropTypes.arrayOf(
+    PropTypes.shape({
+      map: PropTypes.shape({
+        latlng: PropTypes.string.isRequired,
+        requestee: PropTypes.string.isRequired,
+        needrescue: PropTypes.bool,
+        detailrescue: PropTypes.string,
+        is_request_for_others: PropTypes.bool,
+        location: PropTypes.string,
+        requestee_phone: PropTypes.string,
+        needfood: PropTypes.bool,
+        detailfood: PropTypes.string,
+        needcloth: PropTypes.bool,
+        detailcloth: PropTypes.string,
+        needwater: PropTypes.string,
+        detailwater: PropTypes.string,
+        needkit_util: PropTypes.string,
+        detailkit_util: PropTypes.string,
+        needtoilet: PropTypes.string,
+        detailtoilet: PropTypes.string,
+        needmed: PropTypes.string,
+        detailmed: PropTypes.string,
+        needothers: PropTypes.string
+      })
     })
-  }))
+  )
 };
-
 
 export default MainPage;
